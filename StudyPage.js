51a03afcbf4e372426462b596bc033af5dedb3e4
Jeremiah Fenner <jfenner@g.clemson.edu>
2023-06-22 18:33:48 -0700

    study page updates

--- StudyPage.js
+++ StudyPage.js
@@ -21,8 +21,8 @@ var makeItRain = function() {
       backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
     }
   
-    $('.rain.front-row').append(drops);
-    $('.rain.back-row').append(backDrops);
+    $('.rain.front-row'+1).append(drops);
+    $('.rain.back-row'+2).append(backDrops);
   }
   
   makeItRain();
