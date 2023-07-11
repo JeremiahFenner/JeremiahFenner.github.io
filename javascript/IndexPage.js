228ecbd18eb7e3079d01e56ff9796e24b35d993c
Jeremiah Fenner <jfenner@g.clemson.edu>
2023-07-10 17:18:43 -0700

    created animation for about me info
    created css, javascript, and html for the about me animation that appears after clicking the "about" button
    
    issue with body->background display flex so I changed it to flex box to solve the problem

--- javascript/IndexPage.js
+++ javascript/IndexPage.js
@@ -9,12 +9,18 @@ function callClick(){
 
 function pushInfo(){
     // $('#aboutPage').animate({'left':'100px'},"slow");
-    document.getElementById("aboutPage").style.transition = "opacity 700ms ease-in-out";
-    document.getElementById("aboutPage").style.opacity = "1";
+    // document.getElementById("aboutPage").style.transition = "opacity 700ms ease-in-out";
+    // document.getElementById("aboutPage").style.opacity = "1";
+    // $('#aboutPage').animate({"margin-right": 0},"slow");
           
 };
 
 function popInfo(){
-    document.getElementById("aboutPage").style.transition = "opacity 700ms ease-in-out";
-    document.getElementById("aboutPage").style.opacity = "0";
+    // document.getElementById("aboutPage").style.transition = "opacity 700ms ease-in-out";
+    // document.getElementById("aboutPage").style.opacity = "0";
+    // $('#aboutPage').animate({'margin-right': -400},"slow");
+};
+
+function showInfo() {
+        $(".aboutPage").toggleClass('open');
 };
